55c2a0cd433a56d63c45be695324724589187a98
Bipolar Ochka <ochkabipoliar@gmail.com>
2026-02-10 16:36:48 +0200

    Update LiquidityMining.sol

--- contracts/LiquidityMining.sol
+++ contracts/LiquidityMining.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-/* 
-  Educational liquidity mining:
+
   - Stake LP token
   - Rewards accrue, but claim vests linearly over vestDuration
 */ 
