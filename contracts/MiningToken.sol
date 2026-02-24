ff684fecb259ff3a5abab2cbdcd5108e22a42c48
Bipolar Ochka <ochkabipoliar@gmail.com>
2026-02-24 13:58:49 +0200

    Update MiningToken.sol

--- contracts/MiningToken.sol
+++ contracts/MiningToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MiningToken is ERC20 {
     constructor() ERC20("Mining Token", "MIN") {
-        _mint(msg.sender, 10000000 * 10**18); // 10 million tokens
+        _mint(msg.sender, 10000000 * 10**18); 
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
